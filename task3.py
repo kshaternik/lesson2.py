ae66ff5945bcaae37304e82290df9ae50d37a9a1
kshaternik <kseniyashaternik@gmail.com>
2023-01-01 19:55:06 +0300

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,10 @@
+# 3.Задайте список из n чисел, заполненный по формуле (1 + 1/n) ** n и выведите на экран их сумму.
+
+n = int(input('Введите число: ')) 
+
+def sequence(n):
+
+    return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
+        
+print(sequence(n))
+print(round(sum(sequence(n))))
